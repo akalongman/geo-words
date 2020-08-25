6340676694508eca54ac6840cd6552f92b3146fe
Avtandil Kikabidze <akalongman@gmail.com>
2020-08-25 21:02:55 +0400

    Update database structure

--- crawler/structure.sql
+++ crawler/structure.sql
@@ -1,37 +1,35 @@
-CREATE TABLE IF NOT EXISTS `projects` (
-    `id`         INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name`       VARCHAR(1000)    NOT NULL,
-    `created_at` DATETIME         NOT NULL,
+CREATE TABLE `projects` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(1000) NOT NULL,
+    `created_at` timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
     PRIMARY KEY (`id`)
-)
-    ENGINE = InnoDB
-    AUTO_INCREMENT = 1
-    DEFAULT CHARSET = utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `crawls` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `project_id` int unsigned NOT NULL,
+    `url` varchar(1000) NOT NULL,
+    `msg` varchar(1000) DEFAULT NULL,
+    `words` int NOT NULL DEFAULT '0',
+    `status` tinyint(1) NOT NULL DEFAULT '0',
+    `created_at` timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+    PRIMARY KEY (`id`),
+    KEY `crawls_projects_id_fk` (`project_id`),
+    CONSTRAINT `crawls_projects_id_fk` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `crawls` (
-    `id`         INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `project_id` INT(11) UNSIGNED NOT NULL,
-    `url`        VARCHAR(1000)    NOT NULL,
-    `msg`        VARCHAR(100)     NOT NULL DEFAULT '',
-    `words`      INT(10)          NOT NULL DEFAULT '0',
-    `status`     TINYINT(1)       NOT NULL DEFAULT '0',
-    `created_at` DATETIME         NOT NULL,
-    `updated_at` DATETIME         NOT NULL,
-    PRIMARY KEY (`id`)
-)
-    ENGINE = InnoDB
-    AUTO_INCREMENT = 1
-    DEFAULT CHARSET = utf8;
+CREATE TABLE `words` (
+    `word` char(100) NOT NULL,
+    `project_id` int unsigned NOT NULL,
+    `crawl_id` int unsigned NOT NULL,
+    `occurrences` int unsigned NOT NULL DEFAULT '1',
+    `created_at` timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` timestamp(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    PRIMARY KEY (`word`,`project_id`),
+    KEY `words_crawls_id_fk` (`crawl_id`),
+    KEY `words_projects_id_fk` (`project_id`),
+    CONSTRAINT `words_crawls_id_fk` FOREIGN KEY (`crawl_id`) REFERENCES `crawls` (`id`),
+    CONSTRAINT `words_projects_id_fk` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `words` (
-    `word`        CHAR(100)        NOT NULL,
-    `project_id`  INT(11) UNSIGNED NOT NULL,
-    `crawl_id`    INT(11) UNSIGNED NOT NULL,
-    `occurrences` INT(11) UNSIGNED NOT NULL DEFAULT '1',
-    `created_at`  DATETIME         NOT NULL,
-    `updated_at`  DATETIME         NOT NULL,
-    PRIMARY KEY (`word`, `project_id`)
-)
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8;
