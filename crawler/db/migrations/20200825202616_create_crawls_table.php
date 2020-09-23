0e1a9256379de3c14c397e13455400758628682e
Avtandil Kikabidze <akalongman@gmail.com>
2020-09-23 10:21:21 +0400

    Update url column

--- crawler/db/migrations/20200825202616_create_crawls_table.php
+++ crawler/db/migrations/20200825202616_create_crawls_table.php
@@ -12,13 +12,13 @@ final class CreateCrawlsTable extends AbstractMigration
         $table
             ->addColumn('id', 'string', ['limit' => 64])
             ->addColumn('project_id', 'integer', ['signed' => false])
-            ->addColumn('url', 'string', ['limit' => 1000])
+            ->addColumn('url', 'string', ['limit' => 10000])
             ->addColumn('msg', 'text', ['limit' => MysqlAdapter::TEXT_REGULAR, 'null' => true])
             ->addColumn('words', 'integer', ['signed' => false, 'default' => 0])
             ->addColumn('status', 'tinyinteger', ['signed' => false, 'default' => 0])
             ->addColumn('created_at', 'timestamp', ['null' => true, 'precision' => 3])
             ->addColumn('updated_at', 'timestamp', ['null' => true, 'precision' => 3])
-            ->addIndex('url')
+            ->addIndex('url', ['limit' => 1000])
             ->addForeignKey('project_id', 'projects', ['id'])
             ->create();
         $this->execute('ALTER TABLE `crawls` MODIFY COLUMN `created_at` TIMESTAMP(3)
